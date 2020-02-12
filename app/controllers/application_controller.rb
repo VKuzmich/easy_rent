9cbd7a78096a74c740608e989613aae6db334ec3
Vladimir Kuzmich <vjk1976@ukr.net>
2020-02-12 11:53:39 +0200

    modifying registration, passwords and session forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:fullname) }
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:fullname) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:fullname, :current_password, :email) }
   end
 end
