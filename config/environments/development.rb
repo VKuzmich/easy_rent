e42d6fdf3a8ae6cc33d4b5142732e617cd27d98d
Vladimir Kuzmich <vjk1976@ukr.net>
2020-02-13 15:20:14 +0200

    connecting to Sendinblue for confirmation letters

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -60,4 +60,16 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address: 'smtp-relay.sendinblue.com',
+      port: 587,
+      user_name: 'app_kvv@ukr.net',
+      password: '19Hqm37Gwt05sxPD',
+      authentication: 'login',
+      enable_starttls_auto: true
+  }
 end
