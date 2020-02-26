1b4680580de68ee508d85c227b1ad6c96d482a2f
Vladimir Kuzmich <vjk1976@ukr.net>
2020-02-26 23:30:59 +0200

    PayPal payments

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,5 +1,5 @@
 class ReservationsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:notify]
 
   def preload
     room = Room.find(params[:room_id])
@@ -23,11 +23,43 @@ class ReservationsController < ApplicationController
   def create
     @reservation = current_user.reservations.create(reservations_params)
 
-    redirect_to @reservation.room, notice: 'Your reservation has boon created!'
+    if @reservation
+      values = {
+          business: 'sb-mjor01115610@business.example.com',
+          cmd: '_xclick',
+          upload: 1,
+          notify_url: 'http://60a59c19.ngrok.io/notify',
+          amount: @reservation.total,
+          item_name: @reservation.room.listing_name,
+          item_number: @reservation.id,
+          quantity: '1',
+          return: 'http://60a59c19.ngrok.io/your_trips'
+      }
+      redirect_to "https://www.sandbox.paypal.com/cgi-bin/webscr?" + values.to_query
+    else
+      redirect_to @reservation.room, alert: 'Oops, something went wrong...!'
+    end
   end
 
+  protect_from_forgery except: [:notify]
+  def notify
+    params.permit!
+    status = params[:payment_status]
+
+    reservation = Reservation.find(params[:item_number])
+
+    if status = "Completed"
+      reservation.update_attributes status: true
+    else
+      reservation.destroy
+    end
+
+    render nothing: true
+  end
+
+  protect_from_forgery except: [:your_trips]
   def your_trips
-    @trips = current_user.reservations
+    @trips = current_user.reservations.where("status =?", true)
   end
 
   def your_reservations
